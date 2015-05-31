1999aa21872463af1c9c131f579c49b2d5edbb11
Tigrou Ind <tigrou.ind@gmail.com>
2015-05-31 15:55:31 +0200

    Add new episodes

--- js/episodes.js
+++ js/episodes.js
@@ -48,6 +48,51 @@ Wolf.Episodes = [
             { file : "maps/w28.map",    partime : 0.0 },
             { file : "maps/w29.map",    partime : 0.0,  secret : true }
         ]
+    },{ 
+        name : "A Dark Secret",
+        enabled : true,
+        levels : [
+            { file : "maps/w30.map",    partime : 1.5 },
+            { file : "maps/w31.map",    partime : 1.5 },
+            { file : "maps/w32.map",    partime : 2.5 },
+            { file : "maps/w33.map",    partime : 2.5 },
+            { file : "maps/w34.map",    partime : 3.5 },
+            { file : "maps/w35.map",    partime : 2.5 },
+            { file : "maps/w36.map",    partime : 2.0 },
+            { file : "maps/w37.map",    partime : 6.0 },
+            { file : "maps/w38.map",    partime : 0.0 },
+            { file : "maps/w39.map",    partime : 0.0,  secret : true }
+        ]
+    },{ 
+        name : "Trail of the Madman",
+        enabled : true,
+        levels : [
+            { file : "maps/w40.map",    partime : 1.5 },
+            { file : "maps/w41.map",    partime : 1.5 },
+            { file : "maps/w42.map",    partime : 2.5 },
+            { file : "maps/w43.map",    partime : 2.5 },
+            { file : "maps/w44.map",    partime : 3.5 },
+            { file : "maps/w45.map",    partime : 2.5 },
+            { file : "maps/w46.map",    partime : 2.0 },
+            { file : "maps/w47.map",    partime : 6.0 },
+            { file : "maps/w48.map",    partime : 0.0 },
+            { file : "maps/w49.map",    partime : 0.0,  secret : true }
+        ]
+    },{ 
+        name : "Confrontation",
+        enabled : true,
+        levels : [
+            { file : "maps/w50.map",    partime : 1.5 },
+            { file : "maps/w51.map",    partime : 1.5 },
+            { file : "maps/w52.map",    partime : 2.5 },
+            { file : "maps/w53.map",    partime : 2.5 },
+            { file : "maps/w54.map",    partime : 3.5 },
+            { file : "maps/w55.map",    partime : 2.5 },
+            { file : "maps/w56.map",    partime : 2.0 },
+            { file : "maps/w57.map",    partime : 6.0 },
+            { file : "maps/w58.map",    partime : 0.0 },
+            { file : "maps/w59.map",    partime : 0.0,  secret : true }
+        ]
     }
 ];
 
